a74c6b6f34c11a4b5245cece23410a8862dd6e88
Sean Whalen <seanwhalen42@gmail.com>
2016-04-23 21:46:52 -0400

    Added prepared statements

--- databaseQueries.cpp
+++ databaseQueries.cpp
@@ -3,60 +3,70 @@
 #include <string>
 #include <pqxx/pqxx>
 
-pqxx::result getUser(double user_id){//Gets all information on a user given a user id
+pqxx::result getUser(double user_id, pqxx::connection_base &c){//Gets all information on a user given a user id
 	try{
-		pqxx::connection c;
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT * FROM users WHERE user_id = " + user_id + ";");
-		w.commit();
+		std::string s = "SELECT * FROM users WHERE user_id = " + user_id + ";";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_user", s);
+		pqxx::result r = t.prepared("get_user").exec();
+		t.commit();
+
 		return r;
 	}
 	catch (const std::exception &e){
 	}
 }
 
-pqxx::result getUserFromEmail(std::string email){//Gets user from email address
+pqxx::result getUserFromEmail(std::string email, pqxx::connection &c){//Gets user from email address
 	try{
-		pqxx::connection c;
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT * FROM users WHERE email = " + email + ";");
-		w.commit();
+		std::string s = "SELECT * FROM users WHERE email = " + email + ";";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_user_email", s);
+		pqxx::result r = t.prepared("get_user_email").exec();
+		t.commit();
+
 		return r;
 	}
 	catch (const std::exception &e){
 	}
 }
 
-pqxx::result getGroupsFromUser(double user_id){ //Gets all groups a user is a member of
+pqxx::result getGroupsFromUser(double user_id, pqxx::connection &c){ //Gets all groups a user is a member of
 	try{
-		pqxx::connection c;
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT * FROM groups natural join membership WHERE user_id = " + user_id + ";");
-		w.commit();
+		std::string s = "SELECT * FROM groups natural join membership WHERE user_id = " + user_id + ";"
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_user_groups", s);
+		pqxx::result r = t.prepared("get_user_groups").exec();
+		t.commit();
+		
 		return r;
 	}
 	catch (const std::exception &e){
 	}
 }
 
-pqxx::result getGroupInfo(double group_id){//Gets all of a groups info 
+pqxx::result getGroupInfo(double group_id, pqxx::connection &c){//Gets all of a groups info 
 	try{
-		pqxx::connection c;
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT * FROM groups WHERE group_id = " + group_id + ";");
-		w.commit();
+		std::string s = "SELECT * FROM groups WHERE group_id = " + group_id + ";";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_group_info", s);
+		pqxx::result r = t.prepared("get_group_info").exec();
+		t.commit();
+		
 		return r;
 	}
 	catch (const std::exception &e){
 	}
 }
 
-bool createGroup(std::string group_name){//Creates a new group called "group_name" Returns true if it suceeds
+bool createGroup(std::string group_name, pqxx::connection &c){//Creates a new group called "group_name" Returns true if it suceeds
 	try{
-		pqxx::connection c;
-		pqxx::work w(c);
-		pqxx::result r = w.exec("INSERT INTO groups (group_name) VALUES (" + group_name + ");");
-		w.commit();
+		std::string s = "INSERT INTO groups (group_name) VALUES (" + group_name + ");";
+		pqxx::transaction_base t  = transaction_base(c);
+		c.prepare("create_group", s);
+		t.prepared("create_group").exec();
+		t.commit();
+		
 		return true;
 	}
 	catch (const std::exception &e){
@@ -64,12 +74,15 @@ bool createGroup(std::string group_name){//Creates a new group called "group_nam
 	}
 }
 
-pqxx::result getUsersFromGroup(double group_id){
+pqxx::result getUsersFromGroup(double group_id, pqxx::connection &c){
 	try{
-		pqxx::connection c:
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT user_id FROM membership WHERE group_id = " + group_id + ";");
-		w.commit();
+		std::string s  = "SELECT user_id FROM membership WHERE group_id = " + group_id + ";";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_group_users", s);
+		pqxx::result r = t.prepared("get_group_users").exec();
+		t.commit();
+		
+		return r;
 	}
 	catch (const std::exception &e){
 	}
@@ -77,21 +90,24 @@ pqxx::result getUsersFromGroup(double group_id){
 
 pqxx::result getMessages(double group_id){
 	try{
-		pqxx::connection c :
-		pqxx::work w(c);
-		pqxx::result r = w.exec("SELECT * FROM messages WHERE group_id = " + group_id + " ORDER BY message_time;");
-		w.commit();
+		std::string s  = "SELECT * FROM messages WHERE group_id = " + group_id + " ORDER BY message_time;";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("get_messages", s);
+		pqxx::result r = t.prepared("get_messages");
+		t.commit();
 	}
 	catch (const std::exception &e){
 	}
 }
 
-bool addMessage(double group_id, double user_id, std::string message_text){
+bool addMessage(double group_id, double user_id, std::string message_text, pqxx::connection c){
 	try{
-		pqxx::connection c :
-		pqxx::work w(c);
-		pqxx::result r = w.exec("INSERT INTO messages (message_time, user_id, group_id) VALUES (CURRENT_TIMESTAMP, " + user_id + ", " + group_id + ");");
-		w.commit();
+		std::string s = "INSERT INTO messages (message_time, user_id, group_id) VALUES (CURRENT_TIMESTAMP, " + user_id + ", " + group_id + ");";
+		pqxx::transaction_base t = transaction_base(c);
+		c.prepare("add_message", s);
+		pqxx::result r = t.prepared("add_message");
+		t.commit();
+		
 		return true;
 	}
 	catch (const std::exception &e){
